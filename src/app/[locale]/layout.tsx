7c42129d07448021606ca8e60617262efc26c876
Megagoz <omer@megagoz.com>
2026-04-10 19:18:25 +0300

    tag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,13 +39,14 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C778NQYZR5" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WCNQLB6Z');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C778NQYZR5');
           `}
         </Script>
         <script
@@ -84,14 +85,7 @@ export default async function LocaleLayout({
         />
       </head>
       <body className="overflow-x-hidden w-full relative antialiased bg-[#F8FAFC]">
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WCNQLB6Z"
-            height="0" 
-            width="0" 
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+
         <NextIntlClientProvider messages={messages}>
           <div className="flex flex-col w-screen max-w-[100vw] overflow-x-hidden min-h-screen relative">
             {children}
